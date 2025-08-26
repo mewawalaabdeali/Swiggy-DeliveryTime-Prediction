750f590e358fd381654f5aae99b1708fd3f93951
ali <ali_abde37@outlook.com>
2025-08-26 14:05:48 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building ML Time Prediction Project to predict delivery time in minutes',
+    author='AbdeAli',
+    license='',
+)
